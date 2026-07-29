f75a3c53c1ccdb932d7139526a2669a66fc416fb
Clara Perju <claraperju@yahoo.com>
2026-07-29 10:22:53 +0300

    Create db.sql

---
+++ SQL/db.sql
@@ -0,0 +1,5 @@
+-- Creating of the database
+CREATE DATABASE IF NOT EXISTS URB_database_bronze;
+CREATE DATABASE IF NOT EXISTS URB_database_silver;
+CREATE DATABASE IF NOT EXISTS URB_database_gold;
+
